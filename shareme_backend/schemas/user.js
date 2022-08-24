d3a52a7a111d0da3f4acedbf77c16c2c7d8ef05c
Ronny <luyiyun1021@github.com>
2022-08-24 10:22:16 -0400

    Final Version

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,16 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [{
+            name: 'userName',
+            title: 'Username',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        },
+    ]
+}
